e0a49a162777f6c43381493c0b94136fd967d398
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-06-27 13:40:18 +0700

    🔧 Route tweaks

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ Route::get('/', function () {
 
 Route::get('/category', function () {
     $data= array("pops" => App\POPS::get(), "nico" => App\NICO::get(), "toho" => App\TOHO::get(), "sega" => App\SEGA::get() ,"game" => App\GAME::get(), "orig" => App\ORIG::get());
-    return view('page.songbycategory')->with('data', $data);
+    return view('page.category')->with('data', $data);
 });
 
 Route::get('/category/{catname}', function ($catname) {
     if(in_array($catname, array('pops','nico','toho','sega','game','orig'))) {
-        return view('songbycategory')->with('cat', $catname)->with('data', App\strtoupper($catname)::get());
+        return view('page.songsbycategory')->with('cat', $catname)->with('data', App\strtoupper($catname)::get());
     } else {
         return('reject')->with('reason', 4001);
     }
@@ -30,16 +30,16 @@ Route::get('/category/{catname}', function ($catname) {
 
 Route::get('/song/{catname}/{song_name}', function ($catname,$song_name) {
     if(in_array($catname, array('pops','nico','toho','sega','game','orig'))) {
-        return view('song')->with('cat', $catname)->with('data', App\strtoupper($catname)::where('name_jp', $song_name)->get());
+        return view('page.song')->with('cat', $catname)->with('data', App\strtoupper($catname)::where('name_jp', $song_name)->get());
     } else {
         return('reject')->with('reason', 4001);
     }
 });
 
 Route::get('/level', function () {
-    return view('songbylevel');
+    return view('page.level');
 });
 
 Route::get('/level/{level}', function ($level) {
-    return view('level');
+    return view('page.songsbylevel');
 });
