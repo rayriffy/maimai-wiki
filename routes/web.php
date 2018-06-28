5ff615aab299f424037c0a2e6f29509ec9d6e262
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-06-28 14:47:03 +0700

    🐛 Fix routing

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,60 @@ Route::get('/category', function () {
 });
 
 Route::get('/category/{catname}', function ($catname) {
-    if (in_array($catname, ['pops', 'nico', 'toho', 'sega', 'game', 'orig'])) {
-        return view('page.songsbycategory')->with('cat', $catname)->with('data', App\strtoupper($catname)::get());
+    switch ($catname) {
+      case "pops":
+        $res=App\POPS::get();
+        break;
+      case "nico":
+        $res=App\NICO::get();
+        break;
+      case "toho":
+        $res=App\TOHO::get();
+        break;
+      case "sega":
+        $res=App\SEGA::get();
+        break;
+      case "game":
+        $res=App\GAME::get();
+        break;
+      case "orig":
+        $res=App\ORIG::get();
+        break;
+      default:
+        $res=null;
+    }
+    if ($res!=null) {
+        return view('page.songsbycategory')->with('cat', $catname)->with('data', $res);
     } else {
         return('reject')->with('reason', 4001);
     }
 });
 
 Route::get('/song/{catname}/{song_name}', function ($catname, $song_name) {
-    if (in_array($catname, ['pops', 'nico', 'toho', 'sega', 'game', 'orig'])) {
-        return view('page.song')->with('cat', $catname)->with('data', App\strtoupper($catname)::where('name_jp', $song_name)->get());
+    switch ($catname) {
+      case "pops":
+        $res=App\POPS::where('name_jp', $song_name)->get();
+        break;
+      case "nico":
+        $res=App\NICO::where('name_jp', $song_name)->get();
+        break;
+      case "toho":
+        $res=App\TOHO::where('name_jp', $song_name)->get();
+        break;
+      case "sega":
+        $res=App\SEGA::where('name_jp', $song_name)->get();
+        break;
+      case "game":
+        $res=App\GAME::where('name_jp', $song_name)->get();
+        break;
+      case "orig":
+        $res=App\ORIG::where('name_jp', $song_name)->get();
+        break;
+      default:
+        $res=null;
+    }
+    if ($res!=null) {
+        return view('page.song')->with('cat', $catname)->with('data', $res);
     } else {
         return('reject')->with('reason', 4001);
     }
